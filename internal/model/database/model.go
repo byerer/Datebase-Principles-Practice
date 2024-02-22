f167254389bb0b01ec74ef15822e0b880bdb16d1
Lee <1930551858@qq.com>
2024-02-22 19:29:17 +0800

    update model

--- internal/model/database/model.go
+++ internal/model/database/model.go
@@ -4,9 +4,39 @@ import "time"
 
 type User struct {
 	ID        int64  `gorm:"primary_key"`
-	Username  string `gorm:"type:varchar(20);not null" binding:"required"`
-	Password  string `gorm:"type:varchar(60);not null" binding:"required"`
-	Email     string `gorm:"type:varchar(100);not null" binding:"required"`
+	Username  string `gorm:"type:varchar(20);not null"`
+	Password  string `gorm:"type:varchar(30);not null"`
+	Email     string `gorm:"type:varchar(30);not null"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
+}
+
+type Teacher struct {
+	User
+	TeacherName string `gorm:"type:varchar(20);not null"`
+}
+
+type Student struct {
+	User
+	ClassID     int64  `gorm:"not null"`
+	StudentID   int64  `gorm:"not null"`
+	StudentName string `gorm:"type:varchar(20);not null"`
+}
+
+type Essay struct {
+	ID        int64  `gorm:"primary_key"`
+	TeacherID int64  `gorm:"not null"`
+	Topic     string `gorm:"type:text;not null"`
+	Kind      string `gorm:"type:varchar(20);not null"`
+	Standard  string `gorm:"type:text;not null"`
+}
+
+type StudentAnswer struct {
+	ID             int64 `gorm:"primary_key"`
+	StudentID      int64 `gorm:"not null"`
+	EssayID        int64 `gorm:"not null"`
+	ReferenceScore int
+	Score          int
+	Answer         string
+	Comment        string `gorm:"type:text"`
 }
